1abc87ae99012e20cf80cf5f49581cedbde7a411
FarahZaqout <farah.zaqout@gmail.com>
2019-07-19 15:34:22 +0300

    create user model
    #16

--- project/settings.py
+++ project/settings.py
@@ -102,6 +102,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# resolves reverse accessors conflict
+AUTH_USER_MODEL = "users.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+  username = models.CharField(max_length=200)
+  email = models.EmailField(
+    verbose_name = 'email address',
+    max_length = 200,
+    unique = True
+  )
+
+  def __str__(self):
+    return self.username
