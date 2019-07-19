212fb44f35d66d7672e5985a1d7769589d45e8e5
FarahZaqout <farah.zaqout@gmail.com>
2019-07-19 23:53:21 +0300

    adds serializers and views to the users app

--- users/models.py
+++ users/models.py
@@ -4,12 +4,18 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-  username = models.CharField(max_length=200)
+  username = models.CharField(
+    max_length=200,
+    unique = True
+    )
   email = models.EmailField(
     verbose_name = 'email address',
     max_length = 200,
     unique = True
   )
+
+  USERNAME_FIELD = 'username'
+  REQUIRED_FIELDS = ('email', 'password', 'first_name', 'last_name')
 
   def __str__(self):
     return self.username
